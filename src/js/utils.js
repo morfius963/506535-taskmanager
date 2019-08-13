96a0a467e9565e2a7bcf4f43a1bf51ced75ac770
Vitalii <morfius963@gmail.com>
2019-08-13 20:41:36 +0300

    Ох уж этот линтер

--- src/js/utils.js
+++ src/js/utils.js
@@ -10,21 +10,16 @@ export const randomArray = (arr) => {
 };
 
 export const getOverdueFilterCount = (tasks) => tasks.reduce((acc, {dueDate}) =>
-  (new Date(Date.now()).getDate() > new Date(dueDate).getDate() ? acc + 1 : acc)
-  , 0);
+  (new Date(Date.now()).getDate() > new Date(dueDate).getDate() ? acc + 1 : acc), 0);
 
 export const getTodayFilterCount = (tasks) => tasks.reduce((acc, {dueDate}) =>
-  (new Date(dueDate).toDateString() === new Date(Date.now()).toDateString() ? acc + 1 : acc)
-  , 0);
+  (new Date(dueDate).toDateString() === new Date(Date.now()).toDateString() ? acc + 1 : acc), 0);
 
 export const getBooleanFilterCount = (tasks, value) => tasks.reduce((acc, task) =>
-  (task[value] ? acc + 1 : acc)
-  , 0);
+  (task[value] ? acc + 1 : acc), 0);
 
 export const getRepeatingFilterValue = (tasks) => tasks.reduce((acc, {repeatingDays}) =>
-  (Object.keys(repeatingDays).some((day) => repeatingDays[day]) ? acc + 1 : acc)
-  , 0);
+  (Object.keys(repeatingDays).some((day) => repeatingDays[day]) ? acc + 1 : acc), 0);
 
 export const getTagsFilterCount = (tasks) => tasks.reduce((acc, {tags}) =>
-  (tags.size > 0 ? acc + 1 : acc)
-  , 0);
+  (tags.size > 0 ? acc + 1 : acc), 0);
