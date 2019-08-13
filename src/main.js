7b302c0e6ab1a8b3c42c61b6a61d468ef1682629
Vitalii <morfius963@gmail.com>
2019-08-13 15:53:31 +0300

    Изменил логику отрисовки и оживил кнопку LoadMore

--- src/main.js
+++ src/main.js
@@ -4,35 +4,69 @@ import {makeFilterTemplate} from './js/components/filter.js';
 import {makeTaskTemplate} from './js/components/task.js';
 import {makeTaskEditTemplate} from './js/components/task-edit.js';
 import {makeLoadMoreTemplate} from './js/components/load-more-button.js';
-import {makeBoardTemplate} from './js/components/board-container.js';
 import {makeBoardFilterTemplate} from './js/components/board-filters.js';
 
-const CARDS_COUNT = 3;
+import {filters as mainFiltersData} from './js/data.js';
+import {tasks as mainTasksData} from './js/data.js';
 
-const renderComponent = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
+const menuContainer = document.querySelector(`.main__control`);
+const mainSearchContainer = document.querySelector(`.main__search`);
+const mainFilterContainer = document.querySelector(`.main__filter`);
+const mainContentContainer = document.querySelector(`.board`);
+const tasksContainer = document.querySelector(`.board__tasks`);
+
+const taskLoadState = {
+  current: 8,
+  step: 8,
+  max: 20
 };
 
-const mainContainer = document.querySelector(`.main`);
-const menuContainer = mainContainer.querySelector(`.main__control`);
+const renderTasks = (container, HTML) => {
+  container.insertAdjacentHTML(`beforeend`, HTML);
+};
+
+const renderMainFilters = (container) => {
+  container.insertAdjacentHTML(`beforeend`, mainFiltersData
+    .map(makeFilterTemplate)
+    .join(``));
+};
+
+const renderComponent = (container, HTML, place) => {
+  container.insertAdjacentHTML(place, HTML);
+};
 
 const renderMockComponents = () => {
   renderComponent(menuContainer, makeMenuTemplate(), `beforeend`);
-  renderComponent(mainContainer, makeSearchTemplate(), `beforeend`);
-  renderComponent(mainContainer, makeFilterTemplate(), `beforeend`);
-  renderComponent(mainContainer, makeBoardTemplate(), `beforeend`);
+  renderComponent(mainSearchContainer, makeSearchTemplate(), `beforeend`);
 
-  const boardContainer = mainContainer.querySelector(`.board`);
-  const taskListContainer = mainContainer.querySelector(`.board__tasks`);
+  renderMainFilters(mainFilterContainer);
 
-  renderComponent(boardContainer, makeBoardFilterTemplate(), `afterbegin`);
-  renderComponent(taskListContainer, makeTaskEditTemplate(), `beforeend`);
+  renderComponent(mainContentContainer, makeBoardFilterTemplate(), `afterbegin`);
 
-  for (let i = 1; i <= CARDS_COUNT; i++) {
-    renderComponent(taskListContainer, makeTaskTemplate(), `beforeend`);
+  renderTasks(tasksContainer, makeTaskEditTemplate(mainTasksData[0]));
+
+  for (let i = 1; i < taskLoadState.current; i++) {
+    renderTasks(tasksContainer, makeTaskTemplate(mainTasksData[i]));
   }
 
-  renderComponent(boardContainer, makeLoadMoreTemplate(), `beforeend`);
+  renderComponent(mainContentContainer, makeLoadMoreTemplate(), `beforeend`);
 };
 
 renderMockComponents();
+
+const loadMoreBtn = document.querySelector(`.load-more`);
+loadMoreBtn.addEventListener(`click`, () => {
+  let step = taskLoadState.current + taskLoadState.step;
+
+  for (let i = taskLoadState.current; i < step; i++) {
+    renderTasks(tasksContainer, makeTaskTemplate(mainTasksData[i]));
+
+    if (i === taskLoadState.max - 1) {
+      loadMoreBtn.remove();
+      taskLoadState.current = taskLoadState.max;
+      break;
+    }
+  }
+
+  taskLoadState.current = step;
+});
