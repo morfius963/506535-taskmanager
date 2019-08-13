d10130a357d68cd2392d63d2525e55e4b72b0ee6
Vitalii <morfius963@gmail.com>
2019-08-13 15:49:59 +0300

    Теперь функции рендерят шаблон на основании данных, которые она принимает

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,11 +1,9 @@
 export const makeSearchTemplate = () => (
-  `<section class="main__search search container">
-    <input
-      type="text"
-      id="search__input"
-      class="search__input"
-      placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
-    />
-    <label class="visually-hidden" for="search__input">Search</label>
-  </section>`
+  `<input
+    type="text"
+    id="search__input"
+    class="search__input"
+    placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
+  />
+  <label class="visually-hidden" for="search__input">Search</label>`
 );
